eaef0e50423834d47cc925b51677ab3fc817b31d
Somesh99 <someshjoshi1@gmail.com>
2022-03-04 09:51:15 +0530

    first commit

---
+++ src/main/scala/Scala_Collection_Set.scala
@@ -0,0 +1,17 @@
+import scala.collection.immutable.HashSet
+object Scala_Collection_Set {
+  def main(args: Array[String]): Unit = {
+    val name = Set("kapil","somesh","rohit","mohit")
+    val rollnumber = Set("101","102","103","104","105","somesh")
+    var setIntersection = name.intersect(rollnumber)
+//    println("Intersection by using intersect method: "+setIntersection)
+
+//    println("Intersection by using & operator: "+(name & rollnumber))
+      var setUnion = name.union(rollnumber)
+      println(setUnion)
+
+
+
+
+  }
+}
