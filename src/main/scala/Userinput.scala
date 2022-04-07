dfac7533743f37f0aea046b34e7163dfb8043267
Somesh99 <someshjoshi1@gmail.com>
2022-04-07 20:31:31 +0530

    first

---
+++ src/main/scala/Userinput.scala
@@ -0,0 +1,13 @@
+object Userinput {
+
+  def main(args: Array[String]): Unit = {
+        var a=scala.io.StdIn.readInt()
+       //readFloat(),readDouble,readChar(),readLine(), readShort()
+       var b=scala.io.StdIn.readLine()
+       var c=scala.io.StdIn.readChar()
+      println(a)
+      println(b)
+    println(c)
+  }
+
+}
