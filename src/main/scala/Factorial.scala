10d65834d6778ed98723079578909832c876ddbe
Somesh99 <someshjoshi1@gmail.com>
2022-04-07 17:43:39 +0530

    first

---
+++ src/main/scala/Factorial.scala
@@ -0,0 +1,32 @@
+object Factorial
+{
+   def main (args: Array[String]): Unit =
+  {
+    var i=scala.io.StdIn.readInt()
+    fact(i)
+
+  }
+
+  def fact(x : BigInt): Unit =
+  {
+    var y :BigInt =1
+    var k =x
+    while(k>0)
+      {
+        if(x==1)
+          {
+            print(x)
+          }
+        else
+          {
+            y = y * k
+
+          }
+          k=k-1
+
+
+      }
+      print(y)
+  }
+
+}
